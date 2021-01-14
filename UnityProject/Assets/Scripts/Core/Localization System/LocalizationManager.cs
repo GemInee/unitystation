8be7900066848be8df802e7ef624a95e7582bf94
GemInee <33014703+GemInee@users.noreply.github.com>
2021-01-15 00:41:51 +0700

    Start code localization for Items
    Base code.
    Need a json reading code.
    Not ready for testing.

--- UnityProject/Assets/Scripts/Core/Localization System/LocalizationManager.cs
+++ UnityProject/Assets/Scripts/Core/Localization System/LocalizationManager.cs
@@ -13,6 +13,7 @@ namespace Localization
 		public static LocalizationManager instance;
 		public GameObject dropDown;
 		private Dictionary<string, string> localizedText;
+		private Dictionary<string, LocalizedItemData> localizedItemData;
 		private static List<LocalizedText> cacheLocalizedGameObjectsUIComponents;
 		private static List<LocalizedText> cacheLocalizedItems;
 		private static List<LocalizedText> cacheLocalizedStrings;
@@ -43,17 +44,19 @@ namespace Localization
 
 		public void LoadLocalizedText()
 		{
+			//Выбор локализации
 			Dropdown dropdown = dropDown.GetComponent("Dropdown") as Dropdown;
 			int choicedLanguage = dropdown.value;
 
-			string fileName = LocalizedFiles[dropdown.value].Name;
+			//Грузим локализацию для UI
+			string fileNameUI = LocalizedFiles[dropdown.value].Name;
 
 			localizedText = new Dictionary<string, string>();
-			string filePath = Path.Combine(Application.streamingAssetsPath, "Localizations", fileName);
+			string filePathUI = Path.Combine(Application.streamingAssetsPath, "Localizations", fileNameUI);
 
-			if (File.Exists(filePath))
+			if (File.Exists(filePathUI))
 			{
-				string dataJson = File.ReadAllText(filePath);
+				string dataJson = File.ReadAllText(filePathUI);
 				LocalizationData loadedData = JsonUtility.FromJson<LocalizationData>(dataJson);
 
 				for (int i = 0; i < loadedData.items.Length; i++)
@@ -68,13 +71,41 @@ namespace Localization
 			}
 
 			isReady = true;
-
+			//Применяем локализацию для UI
 			foreach (LocalizedText component in cacheLocalizedGameObjectsUIComponents)
 			{
 
 				component.SetLocalizationText((GetLocalizedValue(component.GetKey())));
 
 			}
+
+			//Грузим локализацию для Items
+			string fileNameItems = LocalizedFiles[dropdown.value].Name + "_items";
+
+			//Готовим словарик приёмник для локализаций
+			localizedItemData = new Dictionary<string, LocalizedItemData>();
+			string filePathItems = Path.Combine(Application.streamingAssetsPath, "Localizations", fileNameItems);
+
+			if (File.Exists(filePathItems))
+			{
+				string dataJson = File.ReadAllText(filePathItems);
+				LocalizedItemData loadedData = JsonUtility.FromJson<LocalizedItemData>(dataJson);
+
+				for (int i = 0; i < loadedData.it .Length; i++)
+				{
+					localizedItemData.Add(loadedData.items[i].key, loadedData.items[i].value);
+				}
+
+			}
+			else
+			{
+				Debug.LogError("Cannot find file");
+			}
+
+
+
+
+
 		}
 
 		public string GetLocalizedValue(string key)
---
+++ UnityProject/Assets/Scripts/Core/Localization System/LocalizedItemData.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Localization
+{
+	public class LocalizedItem : MonoBehaviour
+	{
+		public string InitialItemName { get; set; }
+		public string InitialItemDescription { get; set; }
+		public string ExportName { get; set; }
+		public string ExportDescription { get; set; }
+		public string ExportMessage { get; set; }
+	}
+
+	public class LocalizedItemData : MonoBehaviour
+	{
+		public string ItemName { get; set; }
+		public Dictionary<string, LocalizedItem> items { get; set; }
+	}
+}
